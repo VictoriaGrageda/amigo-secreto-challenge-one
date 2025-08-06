587b937fdc8f5c8b12d8dd61c8628325c33c46e5
VictoriaGrageda <victoriagragedavallejos@gmail.com>
2025-08-05 23:07:12 -0400

    funciones requeridas

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function verificarAmigoIngresado(){
+    let amigoIngresado = document.getElementById('amigo').value;
+    if (amigoIngresado == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(amigoIngresado);
+        limpiarCaja();
+        mostrarListaAmigos();
+    }
+}
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+function mostrarListaAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML(`<li>${amigos[i]}</li>`);
+    }
+}
